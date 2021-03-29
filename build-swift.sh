bd083e4b85e936f6dc71c33d157dc281c637b4b5
Anton Pogonets <apogonets@readdle.com>
2021-03-29 19:16:20 +0300

    Remove unused libs

--- build-swift.sh
+++ build-swift.sh
@@ -1,1 +1,1 @@
-SKIP_HARFBUZZ=1 SHARED_ICU=1 SKIP_ICONV=1 ARCHS='armeabi-v7a arm64-v8a x86_64 x86' LIBSUFFIX=swift ./build.sh
+SHARED_ICU=1 ARCHS='armeabi-v7a arm64-v8a x86_64 x86' LIBSUFFIX=swift ./build.sh
